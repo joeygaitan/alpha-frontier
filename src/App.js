7d73638628cfc6671e9e69075fe875ef31674418
Joey Gaitan <joeygaitan707@gmail.com>
2019-10-26 02:47:08 -0700

    added in provider, now going to work on functionality

--- src/App.js
+++ src/App.js
@@ -6,23 +6,51 @@ import AdminLogin from './components/AdminLogin/AdminLogin';
 import AuthenitactedRoute from './higherOrderComponents/AuthenitactedRoute'
 import AdminLandingPage from './components/landingPage/adminLandingPage/adminLandingPage'
 
+//context files
+import AdminContext from './context/admin-context';
+import UserContext from './context/user-context';
+import userContext from './context/user-context';
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
-        return ( 
-            <Router>
-                <div>
-                    <Switch>
-                        <Route exact path = '/' component={LandingPage}/>
-                        <Route path = '/login' component={UserLogin}/>
-                        <Route path='/admin-login' component={AdminLogin}/>
-                        <AuthenitactedRoute path ='/admin-landing-page' component={AdminLandingPage}/>
-                    </Switch>
-                </div>
-            </Router>
+        return (
+            <AdminContext.Provider value={
+                user,
+                pending,
+                item,
+                logIn,
+                logOut,
+                addItem,
+                updateItem,
+                removeItem
+            }>
+                <UserContext value={ 
+                    user,
+                    pending,
+                    item,
+                    signUp,
+                    logIn,
+                    logOut,
+                    addToCart,
+                    removeFromCart,
+                    checkOut
+                }>
+                    <Router>
+                        <div>
+                            <Switch>
+                                <Route exact path = '/' component={LandingPage}/>
+                                <Route path = '/login' component={UserLogin}/>
+                                <Route path='/admin-login' component={AdminLogin}/>
+                                <AuthenitactedRoute path ='/admin-landing-page' component={AdminLandingPage }/>
+                            </Switch>
+                        </div>
+                    </Router>
+                </UserContext>
+            </AdminContext.Provider>
          );
     }
 }
