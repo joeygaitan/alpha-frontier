b8cb326f33635e8f191a943e7249809af0b99f9f
Joey Gaitan <joeygaitan707@gmail.com>
2019-10-29 15:42:17 -0700

    adding in context layout

--- src/App.js
+++ src/App.js
@@ -9,26 +9,19 @@ import AdminLandingPage from './components/landingPage/adminLandingPage/adminLan
 //context files
 import AdminContext from './context/admin-context';
 import UserContext from './context/user-context';
-import userContext from './context/user-context';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            Admin: false,
+            User: false
+        }
     }
     render() { 
+        
         return (
-            <AdminContext.Provider value={
-                user,
-                pending,
-                item,
-                logIn,
-                logOut,
-                addItem,
-                updateItem,
-                removeItem
-            }>
-                <UserContext value={ 
+                <UserContext value={
                     user,
                     pending,
                     item,
@@ -44,14 +37,38 @@ class App extends Component {
                             <Switch>
                                 <Route exact path = '/' component={LandingPage}/>
                                 <Route path = '/login' component={UserLogin}/>
+                                <Route path='/user-login' component={AdminLogin}/>
+                                <AuthenitactedRoute path ='/user-landing-page' component={AdminLandingPage }/>
+                            </Switch>
+                        </div>
+                    </Router>
+                </UserContext>
+            
+         );
+
+         if (this.state.Admin) {
+            <AdminContext.Provider value={
+                user,
+                pending,
+                item,
+                logIn,
+                logOut,
+                addItem,
+                updateItem,
+                removeItem
+            }>
+                <Router>
+                        <div>
+                            <Switch>
+                                <Route exact path = '/' component={LandingPage}/>
+                                <Route path = '/login' component={UserLogin}/>
                                 <Route path='/admin-login' component={AdminLogin}/>
                                 <AuthenitactedRoute path ='/admin-landing-page' component={AdminLandingPage }/>
                             </Switch>
                         </div>
                     </Router>
-                </UserContext>
             </AdminContext.Provider>
-         );
+         }
     }
 }
 
